287ae2f33192eae12e8c460d86f13855c23ca9fb
Isaac Ssemugenyi <isaacpro01@gmail.com>
2020-07-18 04:24:58 +0300

    added product pages and change class to css and views

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,7 +3,27 @@ const router = express.Router();
 
 //Default route
 router.get('/', (req, res)=>{
-    res.render('homepage')
+    res.render('homepage', {title: "Home"})
+})
+
+//fitness route
+router.get('/fitness', (req, res)=>{
+    res.render('fitness', {title: "Fitness"})
+})
+
+//machinery route
+router.get('/machinery', (req, res)=>{
+    res.render('machinery', {title: "Machinery"})
+})
+
+//Furniture route
+router.get('/furniture', (req, res)=>{
+    res.render('furniture', {title: "Furniture"})
+})
+
+//electronics route
+router.get('/electronics', (req, res)=>{
+    res.render('electronics', {title: "Electronics"})
 })
 
 //product list access by admin with full right to edit and delete
