5719890b4d94949e15f04224a7e3a8c74f404437
Isaac Ssemugenyi <isaacpro01@gmail.com>
2021-03-03 16:37:38 +0300

    Modify db connection string

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
     database: 'mongodb://localhost:27017/CovidStores',
+    connectionstring: 'mongodb+srv://isaac:covidstore@covidstore.4epjc.mongodb.net/covidstore?retryWrites=true&w=majority'
     //secret: 'mostsecretwordeverseen'
 }
